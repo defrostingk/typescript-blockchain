2020a492dbdbb90f51accc3248ec82d9e5ac3661
Haedong Kim <haedong1111@gmail.com>
2022-06-27 20:49:50 +0900

    #5.5 Blocks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import { init, exit } from './myPackage';
+import crypto from 'crypto';
 
-init({ debug: true, url: '123' });
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-exit(123);
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    const toHash = `${prevHash}${height}${data}`;
+    return toHash;
+  }
+}
